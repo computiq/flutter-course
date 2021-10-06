83a7d7755a08632c671fb098a9f20557bbee6fcc
Manhal <78895252+Manhalmaath@users.noreply.github.com>
2021-10-07 00:08:27 +0300

    Week_1_Task

--- lib/main.dart
+++ lib/main.dart
@@ -1,249 +1,137 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
-void main() {
-  runApp(const MyApp());
-}
 
-class Contact {
-  String image;
-  String name;
-  String mobileNumber;
+
+class Contacts {
+  String name, phoneNumber, image;
   DateTime date;
   bool isIncoming;
-
-  Contact(this.image, this.name, this.mobileNumber, this.date, this.isIncoming);
+  Contacts(this.name, this.phoneNumber, this.image, this.date, this.isIncoming);
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo 2',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Contacts App'),
-    );
-  }
+  _HomepageState createState() => _HomepageState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 2;
+class _HomepageState extends State<Homepage> {
+  int _selectedIndex = 0;
   static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static late List<Widget> _pages;
-
-  _MyHomePageState() {
-    _pages = [
-      buildContactsList(),
-      buildFavoritesGridView(),
-      // Text('hello'),
-      Text(
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  // ignore: non_constant_identifier_names
+  static late List<Widget> _Pages;
+  _HomepageState() {
+    _Pages = <Widget>[
+      buildCntactsList(),
+      const Text(
+        'Index 1: Business',
+        style: optionStyle,
+      ),
+      const Text(
         'Index 2: School',
+        style: optionStyle,
+      ),
+      const Text(
+        'Index 3: Settings',
         style: optionStyle,
       ),
     ];
   }
-
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
-  var contacts = [
-    Contact(
-      'https://i.pravatar.cc/300',
-      'Ahmed',
-      '71766137347',
-      DateTime.now().add(
-        const Duration(seconds: 3),
-      ),
-      true,
-    ),
-    Contact(
-      'https://i.pravatar.cc/301',
-      'Ali',
-      '71766137347',
-      DateTime.now().add(
-        const Duration(days: 1),
-      ),
-      false,
-    ),
-    Contact(
-      'https://i.pravatar.cc/302',
-      'Kamal',
-      '71766137347',
-      DateTime.now().add(
-        const Duration(days: 3),
-      ),
-      true,
-    ),
-    Contact(
-      'https://i.pravatar.cc/303',
-      'Mohammad',
-      '71766137347',
-      DateTime.now().add(
-        const Duration(days: 5),
-      ),
-      true,
-    ),
-    Contact(
-      'https://i.pravatar.cc/304',
-      'Mohammad',
-      '71766137347',
-      DateTime.now().add(
-        const Duration(days: 5),
-      ),
-      false,
-    ),
-    Contact(
-      'https://i.pravatar.cc/305',
-      'Hussein',
-      '71766137347',
-      DateTime.now().add(
-        const Duration(days: 6),
-      ),
-      false,
-    ),
-    Contact(
-      'https://i.pravatar.cc/306',
-      'Aboud',
-      '71766137347',
-      DateTime.now().add(
-        const Duration(days: 7),
-      ),
-      false,
-    ),
-    Contact(
-      'https://i.pravatar.cc/307',
-      'Osama',
-      '71766137347',
-      DateTime.now().add(
-        const Duration(days: 6),
-      ),
-      false,
-    ),
-  ];
+  Widget buildCntactsList() {
+    var contacts = [
+      Contacts("Manhal", "047894184154", "https://i.pravatar.cc/300",
+          DateTime.now().add(const Duration(days: 3)), true),
+      Contacts("ali", "045959418415", "https://i.pravatar.cc/301",
+          DateTime.now().add(const Duration(days: 4)), false),
+      Contacts("mohamed", "045959424788", "https://i.pravatar.cc/302",
+          DateTime.now().add(const Duration(days: 5)), true),
+      Contacts("mustafa", "045914584788", "https://i.pravatar.cc/303",
+          DateTime.now().add(const Duration(days: 5)), false),
+      Contacts("jaafer", "045975864788", "https://i.pravatar.cc/304",
+          DateTime.now().add(const Duration(days: 5)), false),
+      Contacts("khaled", "045913495788", "https://i.pravatar.cc/305",
+          DateTime.now().add(const Duration(days: 5)), true),
+      Contacts("karrar", "045913495788", "https://i.pravatar.cc/306",
+          DateTime.now().add(const Duration(days: 5)), false),
+      Contacts("diyar", "045913495788", "https://i.pravatar.cc/307",
+          DateTime.now().add(const Duration(days: 5)), true),
+      Contacts("maath", "045913495788", "https://i.pravatar.cc/308",
+          DateTime.now().add(const Duration(days: 5)), false),
+      Contacts("jasem", "045913495788", "https://i.pravatar.cc/309",
+          DateTime.now().add(const Duration(days: 5)), true),
+    ];
 
-  Widget buildFavoritesGridView() {
-    return Column(
-      children: [
-        Text('Favorites'),
-        Divider(thickness: 4,),
-        Expanded(
-          child: GridView.count(
-            crossAxisCount: 3,
-            children: List.generate(5, (index) {
-              var personColor = Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
-                  .withOpacity(1.0);
-              return Center(
-                child: Container(
-                  width: 120,
-                  height: 120,
-                  child: Text(
-                    contacts[index].name[0],
-                    style: TextStyle(fontSize: 40),
-                  ),
-                  alignment: Alignment.center,
-                  decoration:
-                  BoxDecoration(shape: BoxShape.circle, color: personColor),
-                ),
-              );
-            }),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget buildContactItem(Contact _contact) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            CircleAvatar(
-              backgroundImage: NetworkImage(_contact.image),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    _contact.name,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(_contact.mobileNumber),
-                ],
-              ),
-            ),
-            Text(_contact.date.toIso8601String().split('T').first),
-            Expanded(
-              child: Container(),
-            ),
-            if (_contact.isIncoming)
-              Icon(
-                Icons.arrow_downward,
-                color: Colors.red,
-              )
-            else
-              Icon(
-                Icons.arrow_upward,
-                color: Colors.green,
-              )
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildContactsList() {
     return ListView.builder(
-      itemBuilder: (_context, index) {
+      itemCount: contacts.length,
+      itemBuilder: (BuildContext context, int index) {
         return buildContactItem(contacts[index]);
       },
-      itemCount: contacts.length,
     );
+  }
+
+  Widget buildContactItem(Contacts _contact) {
+    return Card(
+        child: Row(
+      children: [
+        CircleAvatar(
+          backgroundImage: NetworkImage(_contact.image),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                _contact.name,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(_contact.phoneNumber),
+            ],
+          ),
+        ),
+        Text(_contact.date.toIso8601String().split("T").first),
+        Expanded(child: Container()),
+        if (_contact.isIncoming)
+          (const Icon(
+            Icons.arrow_upward,
+            color: Colors.green,
+          ))
+        else
+          (const Icon(Icons.arrow_downward, color: Colors.red))
+      ],
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
-        child: _pages[_selectedIndex],
+        child: _Pages[_selectedIndex],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
-            label: 'Recent',
+            label: 'Home',
+            backgroundColor: Colors.red,
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Favorites',
+            icon: Icon(Icons.business),
+            label: 'Business',
+            backgroundColor: Colors.green,
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.access_time_outlined),
-              label: 'School',
-              activeIcon: Icon(Icons.access_time_filled)
+            icon: Icon(Icons.school),
+            label: 'School',
+            backgroundColor: Colors.purple,
           ),
         ],
         currentIndex: _selectedIndex,
@@ -251,5 +139,20 @@ class _MyHomePageState extends State<MyHomePage> {
         onTap: _onItemTapped,
       ),
     );
+  }
+}
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: Homepage());
   }
 }
