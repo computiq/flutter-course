d94369498557160c770c5e59260274190dd6c636
Worood esam <91219550+Wroo11d@users.noreply.github.com>
2021-10-14 12:21:10 +0300

    Create main.dart

---
+++ Task2/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:todo2/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
