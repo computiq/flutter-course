910e9a9e56cd4efc9d3a73dfb201630784510298
Baraa Basim <34444872+BaraaBasim@users.noreply.github.com>
2021-10-16 22:44:10 +0300

    task2

---
+++ ios/android/app/src/main/kotlin/com/example/contacts_01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.contacts_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/lib/models/note.dart
@@ -0,0 +1,4 @@
+class NoteModel {
+  NoteModel(this.content);
+  String content;
+}
